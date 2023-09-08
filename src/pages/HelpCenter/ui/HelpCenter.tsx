6cb7ca75f414bc7d78304a5ac7d519c2bb10f647
Nikita Semenov <script696@yandex.ru>
2023-09-08 14:02:11 +0300

    feat: add faq center

--- src/pages/HelpCenter/ui/HelpCenter.tsx
+++ src/pages/HelpCenter/ui/HelpCenter.tsx
@@ -1,5 +1,4 @@
 import Avatar from "@material-ui/core/Avatar";
-import Badge from "@material-ui/core/Badge";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
@@ -9,15 +8,13 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import HelpIcon from "@material-ui/icons/Help";
 import MailIcon from "@material-ui/icons/Mail";
-import SchoolIcon from "@material-ui/icons/School";
-import SupportIcon from "@material-ui/icons/Support";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { Link as RouterLink } from "react-router-dom";
-import { ReactComponent as HelpSvg } from "../../../shared/assets/help.svg";
-import SvgContainer from "shared/components/SvgContainer/SvgContainer";
 import AdminAppBar from "shared/components/AdminAppBar/AdminAppBar";
 import AdminToolbar from "shared/components/AdminToolbar/AdminToolbar";
+import HelpOutlineOutlinedIcon from "@material-ui/icons/HelpOutlineOutlined";
+import { Box } from "@material-ui/core";
 
 const HelpCenter = () => {
   const { t } = useTranslation();
@@ -25,45 +22,15 @@ const HelpCenter = () => {
   return (
     <React.Fragment>
       <AdminAppBar>
-        <AdminToolbar title={t("help.title")} />
+        <AdminToolbar title={t("admin.help-center.title")} />
       </AdminAppBar>
       <Container maxWidth="xs" sx={{ mt: 3 }}>
-        <SvgContainer>
-          <HelpSvg />
-        </SvgContainer>
+        <Box display={"flex"} justifyContent={"center"}>
+          <HelpOutlineOutlinedIcon style={{ width: "200px", height: "200px" }} />
+        </Box>
       </Container>
       <Grid container spacing={2} sx={{ mt: 3 }}>
-        <Grid item xs={6} lg={3}>
-          <Card>
-            <CardActionArea disabled={true}>
-              <CardHeader
-                avatar={
-                  <Avatar aria-label="Guides icon">
-                    <SchoolIcon />
-                  </Avatar>
-                }
-              />
-              <CardContent>
-                <Badge
-                  badgeContent="Coming soon"
-                  color="primary"
-                  sx={{
-                    "& .MuiBadge-badge": {
-                      top: -8,
-                      right: 10,
-                      whiteSpace: "nowrap",
-                    },
-                  }}
-                >
-                  <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    {t("help.menu.guide")}
-                  </Typography>
-                </Badge>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid item xs={6} lg={3}>
+        <Grid item xs={6}>
           <Card>
             <CardActionArea component={RouterLink} to={`/${process.env.PUBLIC_URL}/admin/faq`}>
               <CardHeader
@@ -75,38 +42,15 @@ const HelpCenter = () => {
               />
               <CardContent>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                  {t("help.menu.faq")}
+                  {t("admin.help-center.faq")}
                 </Typography>
               </CardContent>
             </CardActionArea>
           </Card>
         </Grid>
-        <Grid item xs={6} lg={3}>
+        <Grid item xs={6}>
           <Card>
-            <CardActionArea
-              component="a"
-              href={process.env.REACT_APP_SUPPORT_LINK}
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <CardHeader
-                avatar={
-                  <Avatar aria-label="Support icon">
-                    <SupportIcon />
-                  </Avatar>
-                }
-              />
-              <CardContent>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                  {t("help.menu.support")}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid item xs={6} lg={3}>
-          <Card>
-            <CardActionArea component="a" href={`mailto:${process.env.REACT_APP_CONTACT_MAIL}`}>
+            <CardActionArea component="a" href={`https://t.me/script696`} target={"_blank"}>
               <CardHeader
                 avatar={
                   <Avatar aria-label="Mail icon">
@@ -116,7 +60,7 @@ const HelpCenter = () => {
               />
               <CardContent>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                  {t("help.menu.contact")}
+                  {t("admin.help-center.contact")}
                 </Typography>
               </CardContent>
             </CardActionArea>
---
+++ src/widgets/AppFaq/ui/AppFaq.tsx
@@ -0,0 +1,32 @@
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Typography from "@material-ui/core/Typography";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import React from "react";
+import { Box } from "@material-ui/core";
+import { useTranslation } from "react-i18next";
+import { QUESTIONS } from "widgets/AppFaq/constants/constants";
+
+const AppFaq = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Box>
+      {QUESTIONS.map((question, index) => (
+        <Accordion key={index}>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography component="p" variant="h6">
+              {t(question.title)}
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography color="text.secondary">{t(question.answer)}</Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </Box>
+  );
+};
+
+export default AppFaq;
